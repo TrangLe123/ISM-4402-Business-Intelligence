4f3cd5fbdadfd46169aeb5ca656e8c9e3417592d
TrangLe123 <70179789+TrangLe123@users.noreply.github.com>
2020-09-07 10:24:19 -0400

    Add files via upload

---
+++ Workbook Assignment 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+Location = "/Users/Trang Le/Downloads/all_040_in_01.P1.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
