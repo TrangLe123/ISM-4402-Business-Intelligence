99dfa60a1913dd83ec11bfad8d3bba863a62acbd
TrangLe123 <70179789+TrangLe123@users.noreply.github.com>
2020-09-07 11:42:54 -0400

    Add files via upload

---
+++ Workbook Assignment 1_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import pandas as pd
+Location = "/Users/Trang Le/OneDrive/Desktop/IPE01.xls"
+df = pd.read_excel(Location)
+
+
+# In[11]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
